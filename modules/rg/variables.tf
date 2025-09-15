f8b07db4092ced2be51233db61d01502171faffd
Simon Lam <32254681+ngeegoh@users.noreply.github.com>
2025-09-16 09:26:34 +1000

    Create variables.tf

---
+++ modules/rg/variables.tf
@@ -0,0 +1,12 @@
+variable "rg_location" {
+  #description = "Global settings object (see module README.md)"
+}
+variable "tags" {
+  description = "(Required) Map of tags to be applied to the resource"
+  type        = map(any)
+}
+variable "settings" {}
+variable "resource_group_name" {
+  description = "(Required) The name of the resource group where to create the resource."
+  type        = string
+}
